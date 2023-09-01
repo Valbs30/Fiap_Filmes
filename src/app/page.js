43d79aab114519ef5cae65e8927321cb40718fa2
Valbs30 <lucasvalbusa30@gmail.com>
2023-08-31 23:52:11 -0300

    version1.1

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,29 @@
+import CardFilme from '@/components/CardFilme'
 import Titulo from '@/components/Titulo'
 
 export default function Home() {
+  //mock
+  const filmes = [
+    {
+      titulo: "Star Wars",
+      nota: 9.5,
+      poster: "https://www.themoviedb.org/t/p/w94_and_h141_bestv2/uLlrDUtFG2tKtDcJN6kBznlqqsp.jpg"
+    },
+    {
+      titulo: "Barbie",
+      nota: 9.0,
+      poster: "https://www.themoviedb.org/t/p/w94_and_h141_bestv2/yRRuLt7sMBEQkHsd1S3KaaofZn7.jpg"
+    },
+    {
+      titulo: "Missão Impossível",
+      nota: 9.2,
+      poster: "https://www.themoviedb.org/t/p/w94_and_h141_bestv2/8hjno4uE19pm0qlfUDcM8e5WK13.jpg"
+    }
+  ]
+
+  const barbie = {
+  }
+
   return ( //JSX
     <>
       <nav className="bg-slate-900 p-4 flex gap-3 items-end">
@@ -14,6 +37,12 @@ export default function Home() {
       </nav>
 
       <Titulo>Em Alta</Titulo>
+
+      <section className="flex flex-wrap gap-2">
+        {filmes.map(filme => <CardFilme filme={filme} /> )}
+      </section>
+
+      <Titulo>Lançamentos</Titulo>
       
     </>
   )
